e84de42631264aea162c2d7cd1785d2edd783471
isaevdan <isaevdan@gmail.com>
2014-09-01 14:15:52 +0300

    Console & DB added

---
+++ SteamProject/Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConfigurationManager.ConnectionStrings["SteamProjectConnectionString"].ToString()))
+            {
+                sqlCon.Open();
+            }            
+        }
+    }
+}
