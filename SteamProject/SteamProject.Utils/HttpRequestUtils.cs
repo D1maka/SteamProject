bc6b68f4b3b6a6a82e20a246a51e1bb1da4da7e6
isaevdan <isaevdan@gmail.com>
2014-09-02 10:49:41 +0300

    Utils added

---
+++ SteamProject/SteamProject.Utils/HttpRequestUtils.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace SteamProject.Utils
+{
+    public static class HttpRequestUtils
+    {
+        private static HttpWebRequest GetRequest(string xmlUrl, string proxy, int timeOut)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(xmlUrl);
+
+            if (!string.IsNullOrEmpty(proxy))
+            {
+                WebProxy wp = new WebProxy();
+
+                Uri url = new Uri(proxy);
+                wp.Address = url;
+
+                request.Proxy = wp;
+            }
+
+            if (timeOut < 100)
+                timeOut = 100;
+
+            request.Timeout = timeOut;
+
+            return request;
+        }
+
+        public static string GetValue(string url, string proxy, int timeOut, string userLogin, string userPassword)
+        {
+            try
+            {
+                HttpWebRequest request = GetRequest(url, proxy, timeOut);
+
+                if (!string.IsNullOrEmpty(userLogin))
+                {
+                    request.Credentials = new NetworkCredential(userLogin, userPassword);
+                }
+
+                string result;
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        //Encoding encode = GetCodingRequest(response, proxy, timeOut, userLogin, userPassword);
+                        using (Stream ResponseStream = response.GetResponseStream())
+                        {
+                            using (StreamReader ReadStream = new StreamReader(ResponseStream))
+                            {
+                                result = ReadStream.ReadToEnd();
+                            }
+                        }
+                    }
+                    else
+                        throw new Exception(string.Format("Ошибка соединения! Код ошибки - {0} описание ошибки {1}", response.StatusCode, response.StatusDescription));
+                }
+
+                return result;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ SteamProject/SteamProject.Utils/QueryUtils.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace SteamProject.Utils
+{
+    public static class QueryUtils
+    {
+        public const string keyFilter = "Manitou.Helper.QueryUtils";
+
+        public static NameValueCollection QueryString
+        {
+            get
+            {
+                if (HttpContext.Current == null)
+                    return new NameValueCollection();
+
+                if (HttpContext.Current.Items.Contains(keyFilter))
+                    return (NameValueCollection)HttpContext.Current.Items[keyFilter];
+
+                return HttpContext.Current.Request.QueryString;
+            }
+
+            set
+            {
+                HttpContext.Current.Items[keyFilter] = value;
+            }
+        }
+
+        public static string GetFullQuery
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                NameValueCollection q = QueryString;
+
+                foreach (string key in q.AllKeys)
+                {
+                    sb.Append(key);
+                    sb.Append("=");
+                    sb.Append(q[key]);
+                    sb.Append("&");
+                }
+
+                return sb.ToString().TrimEnd(new char[] { '&' });
+            }
+        }
+
+        public static string GetFullUrl
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("{0}Default.aspx", WebUtils.GetRootHTTP());
+
+                string query = GetFullQuery;
+
+                sb.AppendFormat("{0}{1}", string.IsNullOrEmpty(query) ? "" : "?", query);
+
+                return sb.ToString();
+            }
+        }
+
+        public static string GetValue(string value, string key)
+        {
+            if (string.IsNullOrEmpty(value))
+                return "";
+
+            value = value.TrimStart('?');
+            if (value.Length > 0)
+            {
+                foreach (string parameters in value.Split('&'))
+                {
+                    string[] keyVal = parameters.Split('=');
+                    if (keyVal[0] == key)
+                        return keyVal[1];
+                }
+            }
+
+            return "";
+        }
+    }
+}
